04af5937e4f7c94044eab47e56a82a85eece13d3
Diego Valle-Jones <diegovalle@gmail.com>
2016-01-04 17:45:22 -0600

    hexbin data

---
+++ R/hexbin.R
@@ -0,0 +1,23 @@
+#' Map of the 31 Mexican states plus the Federal District
+#'
+#' A data.frame which contains an hexagonal bin map of all 31 Mexican States plus
+#' the Federal District.
+#'
+#' @docType data
+#' @name #' Hex bin map of the 31 Mexican states plus the Federal District
+#'
+#' A data.frame which contains an hexagonal bin map of all 31 Mexican States plus
+#' the Federal District.
+#'
+#' @docType data
+#' @name mxhexbin.map
+#' @usage data(mxhexbin.map)
+#' @examples
+#' \dontrun{
+#' # render the map with ggplot2
+#' library(ggplot2)
+#'
+#' data(mxhexbin.map)
+#' ggplot(mxhexbin.map, aes(long, lat, group=group)) + geom_polygon(color = "black")
+#' }
+NULL
